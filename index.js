85dec5068bdedb434775c8071c008c1ecb887b83
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-04-13 00:50:21 +0800

    bind 0.0.0.0

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
 const express = require('express');
 
 // Constants
-const PORT = 8080;
-const HOST = 'localhost';
+const PORT = 3000;
+const HOST = '0.0.0.0';
 const OS = require('os');
 const ENV = 'DEV';
 
