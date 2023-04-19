829ee4382bb4c58c5a389a49e2ac042cfa83d7c2
Jazeel Meerasah <jazeel_18@hotmail.com>
2023-04-19 20:38:45 +0800

    Change node 2 to node 3

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const ENV = 'DEV';
 const app = express();
 app.get('/', (req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from Node 2!';
+  const msg = 'Hello from Node 3!';
   res.send(msg);
 });
 
