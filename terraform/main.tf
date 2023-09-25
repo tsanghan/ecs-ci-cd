2b327f04385b8ada0886c881bb9843f070067e9d
chathra <chathraserasinghe89@gmail.com>
2023-09-25 17:37:27 +0800

    quick fixes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 data "aws_vpc" "selected" {
   filter {
     name   = "tag:Name"
-    values = ["sandbox-vpc"]
+    values = ["sctp-sandbox-vpc-vpc"]
   }
 }
 
@@ -50,7 +50,32 @@ module "ecs" {
       assign_public_ip = true
       deployment_minimum_healthy_percent = 100
       subnet_ids = flatten(data.aws_subnets.public.ids)
-      security_group_ids  = ["sg-01adb0fa94b766534"]
+      security_group_ids  = [aws_security_group.allow_sg.id]
     }
+  }
+}
+
+resource "aws_security_group" "allow_sg" {
+  name        = "allow_tls"
+  description = "Allow traffic"
+  vpc_id      = data.aws_vpc.selected.id
+
+  ingress {
+    description = "Allow all"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = [data.aws_vpc.selected.cidr_block]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_sg"
   }
 }
